d871c2dc2aa9ec8672b321b52383896fe8a4f1bc
renasami <drrr0502@gmail.com>
2021-08-13 17:24:55 +0900

    簡易login page 途中

---
+++ app/my-vue-app/src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Hello this is home comp</h1>
+</template>
+<script>
+export default {
+}
+</script>
+<style >
+
+</style>
--- app/my-vue-app/src/components/Login.vue
+++ app/my-vue-app/src/components/Login.vue
@@ -1,13 +1,30 @@
-<template>
-    <div>
+<template height="100%">
+    <div id="login">
         <h1> this is login page </h1>
-        <form method="post">  </form>
-        <form method="post">  </form>
+        <label for="username">fas</label>
+        <input type="text" id="username" v-model="name"> <br>
+        <label for="password">fas</label>
+        <input type="text" id="password" v-model="pas"> <br>
+        <button type="submit">ログイン</button>
     </div>
 </template>
 <script >
 export default{
     name: "Login",
+    data() {
+        return {
+            name: "",
+            password: "",
+        }
+    },
+    methods: {
+        login() {
+        
+        },
+        valid() {
+
+        }
+    }
 }
 
 </script>

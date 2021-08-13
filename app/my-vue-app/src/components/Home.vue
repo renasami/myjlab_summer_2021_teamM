0fa1235e38e794883f6cc881356fa0952e5a2d38
amikudo <amxxkey@gmail.com>
2021-08-13 19:31:47 +0900

    ボタン中央寄せ

--- app/my-vue-app/src/components/Home.vue
+++ app/my-vue-app/src/components/Home.vue
@@ -1,10 +1,46 @@
 <template>
-    <h1>Hello this is home comp</h1>
+    <div>
+        <h1>Hello this is home comp</h1>
+        <Navber/>
+    </div>
 </template>
+
 <script>
+import Navber from './Navber'
 export default {
+    name: 'Home',
+    components: {
+    Navber
+    }
 }
 </script>
-<style >
 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  height: 100vh;
+}
+
+#nav {
+  position: fixed;
+  background-color: #F7F7F7;
+  top:0;
+  width:250px;
+  height:100vh;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+
+}
 </style>

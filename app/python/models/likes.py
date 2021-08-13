9fc41983bceb5127c376ff22b3aad187462ddcec
ohtakaisei <blogsenmonyou@gmail.com>
2021-08-13 13:49:16 +0900

    mdoels file

---
+++ app/python/models/likes.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from .database import Base
+from datetime import datetime
+
+# テーブル定義(DBのモデル)
+# class テーブル名Table(Base):
+    # __tablename__ = 'テーブル名'
+    # 定義されている全てのカラムを記述
+
+
+class LIKESTable(Base):
+    __tablename__ = 'LIKES'
+    ID = Column(Integer, primary_key=True, autoincrement=True)
+    POST_ID = Column(Integer, nullable=False, ForeignKey("POSTS.ID")) 
+    USER_ID = Column(Integer, nullable=False, ForeignKey("USERS.ID"))
+    CREATED_AT = Column(DateTime, default=datetime.now(), nullable=False)
+    UPDATED_AT = Column(DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False)

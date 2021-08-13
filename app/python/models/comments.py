fe0d7858b76ad40181322b5dde8604d25454dbe6
nobuyaaa <anobu4@gmail.com>
2021-08-13 13:46:57 +0900

    commentsとusers.py作成

--- app/python/models/comments.py
+++ app/python/models/comments.py
@@ -9,11 +9,11 @@ from datetime import datetime
     # 定義されている全てのカラムを記述
 
 
-class USERSTable(Base):
-    __tablename__ = 'USERS'
+class COMMENTSTable(Base):
+    __tablename__ = 'COMMENTS'
     ID = Column(Integer, primary_key=True, autoincrement=True)
-    NAME = Column(String(20)) 
-    PASSWORD = Column(String, nullable=False) 
-    PASSWORD_CONFIRMATION = Column(String, nullable=False)
+    COMMENT = Column(String(20)) 
+    POST_ID = Column(Integer, nullable=False, ForeignKey("POSTS.ID")) 
+    USER_ID = Column(Integer, nullable=False, ForeignKey("USERS.ID"))
     CREATED_AT = Column(DateTime, default=datetime.now(), nullable=False)
-    UPDATED_AT = Column(DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False
+    UPDATED_AT = Column(DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False)

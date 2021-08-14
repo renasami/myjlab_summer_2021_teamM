3e49807ef395b5966eac90a10c43d9e88bc366bf
amikudo <amxxkey@gmail.com>
2021-08-14 22:48:36 +0900

    エラー修正

--- app/my-vue-app/src/components/Card.vue
+++ app/my-vue-app/src/components/Card.vue
@@ -9,20 +9,19 @@
         <div class='lower_text'>
           <div class="title">{{ item.title }}</div>
           <div class="icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#231815" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M16 17l5-5-5-5M19.8 12H9M10 3H4v18h6"/></svg>
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#231815" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg></div>
+            <div class='like_button'>
+                <svg v-bind:class="{ after_button: buttonState }" v-on:click="changeState" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#231815" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#231815" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
+              </div>
+            </div>
         </div></div>
         </li>
     </div>
-    <button @click="getResult">button</button>
 </div>
 </template>
 
 <script>
 import axios from 'axios';
-
- //const url = 'http://0.0.0.0:8080/latestposts';
-            // axiosでデータの受け渡し
 axios.get('http://0.0.0.0:8000/latestposts').then(result => console.log(result.data))
 
 export default {
@@ -30,6 +29,7 @@ export default {
    data: function () {
      return {
         list: [],
+        buttonState: false  
      }
    },
 computed: {
@@ -46,10 +46,16 @@ computed: {
     return grouped_list
   }
 },
-methods: {
-        getResult: function() {
+methods:{
+  changeState: function(){
+       
+          //  いいねがクリックされた後の処理
+
+    this.buttonState = !this.buttonState
+  }
+},
+mounted: function(){
             // axiosでデータの受け渡し
-            // this.axios.get('http://0.0.0.0:8000/latestposts').then(response => console.log(response))
             this.axios.get('http://0.0.0.0:8000/latestposts').then(response => 
              {
                const movies = response.data
@@ -63,22 +69,20 @@ methods: {
           });
           console.log(typeof(this.list))
       }
-          
-        }
 }
 
 </script>
 
 <style scoped>
 .card {
-  width:82%;
+  width:80%;
   position: absolute;
   right: 0;
   margin-top: 50px;
 }
 
 .cards {
-  width:300px;
+  width:320px;
   height:250px;
   margin: 20px 20px;
   display: inline-block;
@@ -94,7 +98,7 @@ methods: {
 
 .upper_video {
   width:100%;
-  height:60%;
+  height:75%;
   background-color:#F7F7F7;
   box-shadow: 0px 2px 6px 0 rgba(0,0,0,0.2);
   border-radius: 3px;
@@ -102,7 +106,7 @@ methods: {
 
 .lower_text {
   width:100%;
-  height:40%;
+  height:25%;
   margin-top:10px;
   display: flex;
   justify-content: space-between;
@@ -115,4 +119,14 @@ methods: {
 .title {
   font-size: 1.2em;
 }
+
+.after_button {
+  fill: #E36C9E;
+  stroke: #E36C9E;
+}
+
+.like_button {
+  float: left;
+}
+
 </style>
--- app/python/main.py
+++ app/python/main.py
@@ -50,7 +50,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins= origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]

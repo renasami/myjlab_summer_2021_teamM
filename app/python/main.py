f29bb9feb4e7b04a6713e518efeb7e922ebf2424
nobuyaaa <anobu4@gmail.com>
2021-08-15 01:07:36 +0900

    a

--- app/python/main.py
+++ app/python/main.py
@@ -474,9 +474,6 @@ def get_Oneyoutube(db: Session = Depends(get_db)):
 
     return trueURL
 
-
-
-
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0",port=8000,reload=True)

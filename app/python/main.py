5dda491e1f277c6206868b1728106cb145c4cb2f
renasami <drrr0502@gmail.com>
2021-08-17 14:28:55 +0900

    追記

--- app/python/main.py
+++ app/python/main.py
@@ -64,55 +64,8 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-
-
-
-@app.get("/userloginlist")
-def get_user(db: Session = Depends(get_db)):
-    USER_LOGIN_LIST = crud.get_login_list(db)
-    return USER_LOGIN_LIST
-
-
-
-#ユーザー一覧
-@app.get('/User')
-def get_login_list(db: Session = Depends(get_db)):
-    user = crud.get_login_list(db)
-    for row in user:
-        d = row.__dict__
-    return d['MAIL']
-
-
-class UserInfo(BaseModel):
-    mail: str
-    password: str
-
-
-    
-@app.post('/login/')
-def login_try(form:UserInfo, user_id :Optional[int] = Cookie(None),db: Session = Depends(get_db)):
-
-    print(form)
-    can_login = crud.try_login(form,db)
-
-    if can_login:
-        users = crud.search_userid(db, form.mail)
-        print(users)
-        # print(Cookie[0])
-        # session['login'] = form.mail
-        return True, {"user_id" : users[0][0]}
-    return False
-
-
-    
   
-#新規会員登録
-@app.post('/register/')
-def create_user(user: schemas.UsersCreate, db: Session = Depends(get_db)):
-    db_user  = crud.get_user_by_email(db, mail=user.mail)
-    if db_user != None:
-        raise HTTPException(status_code=400, detail="このメールアドレスは会員登録が完了しています")
-    return crud.create_user(db=db, user=user)
+
 
 class PostInfo(BaseModel):
     # thumbnail_id: str
